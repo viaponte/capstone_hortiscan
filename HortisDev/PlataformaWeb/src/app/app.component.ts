85a8a6ebeda8ccf25caafe65c67cadc648d71ef8
viaponte <vi.aponte@duocuc.cl>
2024-08-22 21:17:12 -0400

    Se inicializa migracion de HTML puro a Angular (por corregir)

--- HortisDev/PlataformaWeb/src/app/app.component.ts
+++ HortisDev/PlataformaWeb/src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./pages/common/header/header.component";
+import { FooterComponent } from "./pages/common/footer/footer.component";
+import { Title } from '@angular/platform-browser';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'PlataformaWeb';
+export class AppComponent implements OnInit {
+
+  constructor(
+    private titleService: Title,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let child = this.activatedRoute.firstChild;
+        while (child?.firstChild) {
+          child = child.firstChild;
+        }
+        return child?.snapshot.data['title'] || 'HortiScan'
+      })
+    ).subscribe((pageTitle: string) => {
+      this.titleService.setTitle(`HortiScan - ${pageTitle}`)
+    });
+  }
 }
