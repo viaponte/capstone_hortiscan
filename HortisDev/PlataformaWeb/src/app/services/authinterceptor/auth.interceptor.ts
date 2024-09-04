795f259c7d595a290a318f59b8603ff0ff5d8f98
viaponte <vi.aponte@duocuc.cl>
2024-09-03 23:15:39 -0400

    Se realiza auth.interceptor, authservice y se deja en funcionamiento login con token

---
+++ HortisDev/PlataformaWeb/src/app/services/authinterceptor/auth.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../authservice/authservice.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}  // Aquí también
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authService.getToken();
+    if (token) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+    return next.handle(req);
+  }
+}
