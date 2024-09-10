2c323e0a23a82b33ef9bdfee48ba72e4481d0a85
viaponte <vi.aponte@duocuc.cl>
2024-09-09 21:52:16 -0300

    Correcciones y registro de usuario en funcionamiento

--- HortisDev/PlataformaWeb/src/app/app.routes.ts
+++ HortisDev/PlataformaWeb/src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 import { ErrorComponent } from './pages/error/error.component';
-import { MainComponent } from './pages/main/main.component';
 import { FolderComponent } from './pages/folder/folder.component';
 import { FormComponent } from './pages/form/form.component';
+import { MainComponent } from './pages/main/main.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/registrarse', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, data: { title: 'Login' } },
   { path: 'menu', component: MainComponent, data: { title: 'Menu' } },
   { path: 'carpeta', component: FolderComponent, data: { title: 'Carpeta' } },
   { path: 'formulario', component: FormComponent, data: { title: 'Formulario' } },
+  { path: 'registrarse', component: RegisterComponent, data: { title: "Registrarse" } },
   { path: '**', component: ErrorComponent, data: { title: 'Error' }}
 ];
