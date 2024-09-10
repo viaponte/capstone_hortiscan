2054db634e38b12e0d2c15ace1a81b57961ff9c8
lucrodriguezc <luc.rodriguezc@duocuc.cl>
2024-09-10 02:09:23 -0300

    Eliminación de Interfaz Ciren y PlatMobile

--- HortisDev/PlataformaMobile/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'PlataformaMobile',
-  webDir: 'www'
-};
-
-export default config;
