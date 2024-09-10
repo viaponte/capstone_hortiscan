867d6ad003e724b8212ffe847e80389eaa4975d4
cristianlara-ss <javierlaraflores@gmail.com>
2024-09-09 22:08:28 -0300

    no funciona vista login

--- HortisDev/PlataformaMobile/src/main.ts
+++ HortisDev/PlataformaMobile/src/main.ts
@@ -2,13 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
-import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';  // Rutas configuradas en app.routes
+import { AppComponent } from './app/app.component';  // Componente raíz de la aplicación
 
+// Iniciar la aplicación utilizando AppComponent como raíz y con las rutas y páginas independientes
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },  // Estrategia de reutilización de rutas de Ionic
+    provideIonicAngular(),  // Provee los componentes y servicios de Ionic
+    provideRouter(routes, withPreloading(PreloadAllModules)),  // Configura las rutas con pre-carga de módulos
   ],
 });
