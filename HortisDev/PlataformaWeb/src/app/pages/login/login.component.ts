079a66fb805476fc1eb938cac91c1adfb14634c8
viaponte <vi.aponte@duocuc.cl>
2024-09-11 15:39:28 -0300

    correcciones de registro

--- HortisDev/PlataformaWeb/src/app/pages/login/login.component.ts
+++ HortisDev/PlataformaWeb/src/app/pages/login/login.component.ts
@@ -15,8 +15,8 @@ import { AuthService } from '../../services/authservice/authservice.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  username: string = 'lucas'; 
-  password: string = 'lucas';
+  username: string = '';
+  password: string = '';
 
   constructor(private router: Router, private authService: AuthService) {}
 
