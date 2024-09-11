ce136559ea4beb99498f43ac5516fd69bf1324f2
cristianlara-ss <javierlaraflores@gmail.com>
2024-09-11 01:39:39 -0300

    Actualización del Diseño web

--- HortisDev/PlataformaWeb/src/app/pages/main/main.component.ts
+++ HortisDev/PlataformaWeb/src/app/pages/main/main.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../shared/common/header/header.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
