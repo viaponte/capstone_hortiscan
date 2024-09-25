a41c2a104649a52501466c59bd8c072941a35157
lucrodriguezc <luc.rodriguezc@duocuc.cl>
2024-09-25 00:11:42 -0300

    pequeño rediseño a main, folder y header

--- HortisDev/PlataformaWeb2/src/app/shared/common/header/header.component.ts
+++ HortisDev/PlataformaWeb2/src/app/shared/common/header/header.component.ts
@@ -1,20 +1,37 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router, Event } from '@angular/router';
 import { AuthService } from '../../../services/authservice/authservice.service';
 import { FooterComponent } from '../footer/footer.component';
+import { Location, CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(private loginService: AuthService, private router: Router) {}
+  showBackButton: boolean = true;
+
+  constructor(private loginService: AuthService, private router: Router, private location: Location) {
+    // Escuchar los cambios de ruta y actualizar showBackButton
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: Event) => {
+        if (event instanceof NavigationEnd) {
+          this.showBackButton = event.url !== '/menu';
+        }
+      });
+  }
 
   logout() {
     this.loginService.logout();
-    this.router.navigate(['/login'])
+    this.router.navigate(['/login']);
+  }
+
+  goBack(): void {
+    window.history.back();
   }
 }
