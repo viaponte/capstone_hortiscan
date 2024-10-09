6a3795d116cf04fd958983582d581bdd0eb3240a
viaponte <vi.aponte@duocuc.cl>
2024-10-08 21:57:57 -0300

    Se implementa previsualización de archivos word mediante generación de PDF

--- HortisDev/PlataformaWeb2/src/app/services/usuarioservice/usuario.service.ts
+++ HortisDev/PlataformaWeb2/src/app/services/usuarioservice/usuario.service.ts
@@ -19,31 +19,27 @@ export class UsuarioService {
   // Método para crear la carpeta
   crearCarpeta(username: string, folderName: string): Observable<any> {
     const url = `${apiUrl}/api/usuario/${username}/crear-carpeta`;
-    
+
     // Asegúrate de enviar un objeto con el nombre de la carpeta como string
     const body = { folderName: folderName };
-  
+
     return this.http.post(url, body);
   }
 
   // Método para obtener las carpetas del usuario
   getCarpetas(username: string): Observable<CarpetaDTO[]> {
-    return this.http.get<CarpetaDTO[]>(`${ apiUrl }/api/usuario/${username}/carpetas`);
+    return this.http.get<CarpetaDTO[]>(`${apiUrl}/api/usuario/${username}/carpetas`);
   }
 
   // Método para obtener el contenido de una carpeta específica
   getCarpetaContenido(username: string, nombreCarpeta: string): Observable<string[]> {
-    return this.http.get<string[]>(`${ apiUrl }/api/usuario/${username}/carpeta/${nombreCarpeta}/contenido`);
+    return this.http.get<string[]>(`${apiUrl}/api/usuario/${username}/carpeta/${nombreCarpeta}/contenido`);
   }
 
-  getImagePath(nombreCarpeta: string, fileName: string): Observable<string> {
+  getImagePath(nombreCarpeta: string, fileName: string): Observable<Blob> {
     const request = `${apiUrl}/api/usuario/${this.username}/carpeta/${nombreCarpeta}/archivo/${fileName}`;
   
-    return this.http.get(request, { responseType: 'blob' }).pipe(
-      map(blob => {
-        return URL.createObjectURL(blob);
-      })
-    );
+    return this.http.get(request, { responseType: 'blob' }); // Cambiamos a 'blob'
   }
 
   // Método para eliminar carpeta y su contenido
@@ -59,4 +55,12 @@ export class UsuarioService {
 
     return this.http.delete(request);
   }
+
+  // Método para obtener el PDF de un archivo Word
+  getPdfPath(nombreCarpeta: string, fileName: string): Observable<Blob> {
+    const request = `${apiUrl}/api/usuario/${this.username}/carpeta/${nombreCarpeta}/archivo/${fileName}/pdf`;
+
+    return this.http.get(request, { responseType: 'blob' });
+  }
+
 }
