c3b84556442ac76063284fe63b4eaa3b5a1e4f38
viaponte <vi.aponte@duocuc.cl>
2024-10-08 21:57:57 -0300

    Se implementa previsualización de archivos word mediante generación de PDF

--- HortisDev/PlataformaWeb2/src/app/pages/folder/folder.component.ts
+++ HortisDev/PlataformaWeb2/src/app/pages/folder/folder.component.ts
@@ -8,6 +8,7 @@ import { CommonModule } from '@angular/common';
 import { forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { SyncService } from '../../services/syncservice/sync.service';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-folder',
@@ -21,9 +22,11 @@ export class FolderComponent implements OnInit {
   imagenesMap: { [key: string]: string } = {}; // Mapa para almacenar las URL de las imágenes
   nombreCarpeta: string = '';  // Variable para almacenar el nombre de la carpeta
   username: string | null = '';  // Variable para almacenar el nombre de usuario
-  selectedFile: string | null = null; // Archivo seleccionado para mostrar en el modal
+  selectedFile: SafeResourceUrl | null = null;  // Archivo seleccionado para mostrar en el modal
 
-  constructor(private route: ActivatedRoute, private usuarioService: UsuarioService, private authService: AuthService, private syncService: SyncService) {
+  constructor(private route: ActivatedRoute, private usuarioService: UsuarioService,
+    private authService: AuthService, private syncService: SyncService,
+    private sanitizer: DomSanitizer) {
     this.username = this.authService.getUsername();
   }
 
@@ -54,20 +57,55 @@ export class FolderComponent implements OnInit {
   loadImages() {
     const imageObservables = this.contenidoCarpeta.map(file =>
       this.usuarioService.getImagePath(this.nombreCarpeta, file).pipe(
-        map(imageUrl => ({ fileName: file, imageUrl }))
+        map((imageBlob) => {
+          // Convertir el Blob en una URL antes de almacenarlo
+          const imageUrl = URL.createObjectURL(imageBlob);
+          return { fileName: file, imageUrl };
+        })
       )
     );
 
     forkJoin(imageObservables).subscribe(images => {
       images.forEach(img => {
-        this.imagenesMap[img.fileName] = img.imageUrl;
+        this.imagenesMap[img.fileName] = img.imageUrl; // Guardamos la URL en lugar del Blob
       });
     });
   }
 
+
+  selectedFileExtension: string | null = null;  
+
   // Método para abrir modal
   openModal(file: string): void {
-    this.selectedFile = this.imagenesMap[file]; // Usa la URL de imagen pre-cargada
+    const fileExtension = file.split('.').pop()?.toLowerCase();
+    this.selectedFileExtension = fileExtension ?? null;  // Guardamos la extensión del archivo o null si no existe
+  
+    // Si es una imagen
+    if (fileExtension === 'jpg' || fileExtension === 'png' || fileExtension === 'jpeg' || fileExtension === 'gif') {
+      this.usuarioService.getImagePath(this.nombreCarpeta, file).subscribe(
+        (imageBlob) => {
+          const fileURL = URL.createObjectURL(imageBlob);  // Crear URL desde Blob
+          this.selectedFile = this.sanitizer.bypassSecurityTrustResourceUrl(fileURL);  // Sanitizamos la URL
+        },
+        (error) => {
+          console.error('Error al cargar la imagen:', error);
+        }
+      );
+    }
+    // Si es un archivo Word
+    else if (fileExtension === 'docx' || fileExtension === 'doc') {
+      this.usuarioService.getPdfPath(this.nombreCarpeta, file).subscribe(
+        (pdfBlob) => {
+          const fileURL = URL.createObjectURL(pdfBlob);  // Crear URL desde Blob
+          this.selectedFile = this.sanitizer.bypassSecurityTrustResourceUrl(fileURL);  // Sanitizamos la URL
+        },
+        (error) => {
+          console.error('Error al cargar el PDF:', error);
+        }
+      );
+    } else {
+      console.error('Formato de archivo no soportado:', fileExtension);
+    }
   }
 
   // Método que retorna la ruta de la imagen
