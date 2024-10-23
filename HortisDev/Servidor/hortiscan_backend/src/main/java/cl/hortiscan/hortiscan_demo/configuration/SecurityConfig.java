31470b790a2645b083116ecbc8792cd5065e36c2
viaponte <vi.aponte@duocuc.cl>
2024-10-23 15:28:24 -0300

    Ni idea

--- HortisDev/Servidor/hortiscan_backend/src/main/java/cl/hortiscan/hortiscan_demo/configuration/SecurityConfig.java
+++ HortisDev/Servidor/hortiscan_backend/src/main/java/cl/hortiscan/hortiscan_demo/configuration/SecurityConfig.java
@@ -45,12 +45,8 @@ public class SecurityConfig {
             .authorizeRequests(authorize -> authorize
                     .requestMatchers("/api/auth/login", 
                     "/api/auth/register", "sync-carpetas-imagenes", 
-<<<<<<< HEAD
-                    "/api/document/download/**", "/api/document/callback","/api/notificaciones/**").permitAll()
-=======
                     "/api/document/download/**", "/api/document/callback", 
-                    "/api/convert/**").permitAll()
->>>>>>> ac98626d996d8577121e5062e2dda8ed05d36347
+                    "/api/convert/**, /api/notificaciones/**").permitAll()
                     .anyRequest().authenticated()
             )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
